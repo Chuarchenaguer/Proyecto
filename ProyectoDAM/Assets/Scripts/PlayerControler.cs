07094316e5ce9186b6e61f427912358a1b01e825
Chuarchenaguer <carliski@gmail.com>
2021-05-16 12:10:53 +0200

    Movimiento Player completado con MoveToWard. Animaciones movimiento Player.

--- ProyectoDAM/Assets/Scripts/PlayerControler.cs
+++ ProyectoDAM/Assets/Scripts/PlayerControler.cs
@@ -3,51 +3,90 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerControler : MonoBehaviour
-{
-    // Start is called before the first frame update
-    //Vector3 puntero;
-    public float speed;
-    //public GameObject target;
+    {
+    public int speed;
     Vector3 target;
+    Vector3 posToGo;
 
     void Start()
     {
+        target = transform.position;
         //gameObject.GetComponent<Transform>().position = new Vector3(20, 0, 0);
         //gameObject.transform.position = new Vector3(gameObject.transform.position.x + 20, gameObject.transform.position.y, gameObject.transform.position.z);
         //puntero = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        /*if (this.gameObject.transform.position.x == target.x)
-        {
-            this.gameObject.transform.position = new Vector3(target.x, 0, 0);
-        }*/
-
-        //gameObject.transform.position = new Vector3(gameObject.transform.position.x + 50f * Time.deltaTime, gameObject.transform.position.y, gameObject.transform.position.z);
-        //gameObject.transform.Translate(50f*Time.deltaTime, 0, 0);
         if (Input.GetMouseButtonUp(0))
         {
-            target = Camera.main.ScreenToWorldPoint(new Vector3(
-            Input.mousePosition.x,
-            0f,
-            0f
-            ));
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         }
-        if (target.x < gameObject.transform.position.x) //movimiento hacía la izquierda
-        {
-            this.gameObject.transform.Translate(-speed * Time.deltaTime, 0, 0);
 
-        } else if (target.x > gameObject.transform.position.x) //movimiento hacía la derecha
+        posToGo = new Vector3(target.x, transform.position.y, transform.position.z);
+        transform.position = Vector3.MoveTowards(transform.position, posToGo, speed * Time.deltaTime);
+
+        if (posToGo.x < gameObject.transform.position.x) //movimiento hacía la izquierda
         {
-            this.gameObject.transform.Translate(speed * Time.deltaTime, 0, 0);
-        } else if ((target.x == gameObject.transform.position.x))
+            gameObject.GetComponent<Animator>().SetBool("moving", true);
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+        }
+        else if (target.x > gameObject.transform.position.x) //movimiento hacía la derecha
         {
-            this.gameObject.transform.position = new Vector3(target.x, 0, 0);
+            gameObject.GetComponent<Animator>().SetBool("moving", true);
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
+        }
+
+            if (transform.position == posToGo)
+        {
+            gameObject.GetComponent<Animator>().SetBool("moving", false);
         }
     }
 }
+/*if (this.gameObject.transform.position.x == target.x)
+{
+    this.gameObject.transform.position = new Vector3(target.x, 0, 0);
+}*/
+
+//gameObject.transform.position = new Vector3(gameObject.transform.position.x + 50f * Time.deltaTime, gameObject.transform.position.y, gameObject.transform.position.z);
+//gameObject.transform.Translate(50f*Time.deltaTime, 0, 0);
+
+/* //OLD
+if (Input.GetMouseButtonUp(0))
+{
+    target = Camera.main.ScreenToWorldPoint(new Vector3(
+    Input.mousePosition.x,
+    0f,
+    0f
+    ));
+}
+
+if (target.x < gameObject.transform.position.x) //movimiento hacía la izquierda
+{
+    gameObject.transform.Translate(-speed * Time.deltaTime, 0, 0);
+    gameObject.GetComponent<Animator>().SetBool("moving", true);
+    gameObject.GetComponent<SpriteRenderer>().flipX = false;
+}
+else if (target.x > gameObject.transform.position.x) //movimiento hacía la derecha
+{
+    gameObject.transform.Translate(speed * Time.deltaTime, 0, 0);
+    gameObject.GetComponent<Animator>().SetBool("moving", true);
+    gameObject.GetComponent<SpriteRenderer>().flipX = true;
+}
+
+
+if ((gameObject.transform.position.x == target.x)) //parado
+{
+    gameObject.transform.Translate(0, 0, 0);
+    gameObject.transform.position = new Vector3(target.x, 0, 0);
+    gameObject.GetComponent<Animator>().SetBool("moving", false);
+}
+}  
+} */
+
+
+
+
 
 /*Debug.Log("hgola");
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
