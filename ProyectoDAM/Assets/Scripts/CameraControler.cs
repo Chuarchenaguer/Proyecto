8a9cc994e815b957fcd48c8692658a0fcfdb730d
Chuarchenaguer <carliski@gmail.com>
2021-05-18 22:24:10 +0200

    CameraController. Escenario y Layers.

---
+++ ProyectoDAM/Assets/Scripts/CameraControler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControler : MonoBehaviour
+{
+
+    public int speed;
+    GameObject player;
+    Vector3 vel = Vector3.zero;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player1");
+    }
+
+    
+    void FixedUpdate()
+    {
+        Vector3 posPlayer = player.transform.position;
+
+        transform.position = Vector3.SmoothDamp(transform.position, posPlayer, ref vel, 0.15f);
+        
+    }
+}
---
+++ ProyectoDAM/Assets/Scripts/Pilares.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pilares : MonoBehaviour
+{
+    private SpriteRenderer _spriteRenderer;
+
+    void Start()
+    {
+        _spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        _spriteRenderer.color = new Color(1f, 1f, 1f, .75f);
+    }
+}
